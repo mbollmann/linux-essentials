61561ee4a4cfddf0d69c77a3aa7ab8e609b2defe
Marcel Bollmann <marcel@bollmann.me>
2021-06-07 13:05:28 +0200

    Change Joplin to GUI desktop app

--- install-non-packaged-programs.sh
+++ install-non-packaged-programs.sh
@@ -15,20 +15,18 @@ if [ $(id -u) = 0 ]; then
     exit 1
 fi
 
+if ! [ -f ~/.joplin/Joplin.AppImage ]; then
+    echo ">>> Installing Joplin for Desktop [via custom installer]"
+    wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+fi
+
 if which npm >/dev/null; then
     # Install global packages in user directory
     if ! [ -d ~/.npm-global ]; then
         mkdir ~/.npm-global
         npm config set prefix '~/.npm-global'
-        export NPM_CONFIG_PREFIX=~/.npm-global
     fi
-
-    # Joplin
-    if ! which joplin >/dev/null; then
-        echo ">>> Installing Joplin [via npm]"
-        npm install -g joplin
-    fi
-
+    export NPM_CONFIG_PREFIX=~/.npm-global
 else
     echo "!!! npm isn't installed; skipping installation of npm packages."
 fi
