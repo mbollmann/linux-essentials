001241babfea9cef410ac13f134a6a840fe6c602
Marcel Bollmann <marcel@bollmann.me>
2023-03-09 11:00:27 +0100

    Add pet installation

--- install-non-packaged-programs.sh
+++ install-non-packaged-programs.sh
@@ -21,20 +21,42 @@ if ! [ -f ~/.joplin/Joplin.AppImage ]; then
 fi
 
 if ! which starship >/dev/null; then
+    echo ">>> Installing starship [via starship.rs/install.sh]"
     curl -sS https://starship.rs/install.sh | sh
 fi
 
 if ! which xcape >/dev/null; then
+    echo ">>> Installing xcape [via github.com/alols/xcape]"
     sudo dnf install git gcc make pkgconfig libX11-devel libXtst-devel libXi-devel
     THISDIR=$(pwd)
     cd /tmp
     git clone https://github.com/alols/xcape.git
-    cd xcape
-    make
+    cd /tmp/xcape
     sed -i 's/^PREFIX=.*/PREFIX=\/usr\/local/' Makefile
     sed -i 's/^MANDIR.*/MANDIR?=\/man\/man1/' Makefile
+    make
     sudo make install
     cd $THISDIR
+    if which xcape >/dev/null; then
+        echo ">>> Successfully installed xcape"
+        rm -rf /tmp/xcape
+    else
+        echo "!!! Installation of xcape failed"
+    fi
+fi
+
+if ! which pet >/dev/null; then
+    if ! which jq >/dev/null; then
+        echo "!!! Installation of pet requires jq; skipping"
+    else
+        echo ">>> Installing pet [via github.com/knqyf263/pet]"
+        petrpm=$(curl -s https://api.github.com/repos/knqyf263/pet/releases/latest | jq '.assets[] | select(.name|match("linux_amd64.rpm$")) | .browser_download_url' | tr -d \")
+        if [[ -z "$petrpm" ]]; then
+            echo "!!! Installation of pet failed: no linux_amd64.rpm found (maybe the latest release doesn't have one?)"
+        else
+            sudo dnf install -y "$petrpm" && echo ">>> Successfully installed pet"
+        fi
+    fi
 fi
 
 if which npm >/dev/null; then
@@ -51,22 +73,8 @@ fi
 if which python >/dev/null; then
     python -m pip install --upgrade pip
     python -m pip install --user virtualfish
-    python -m pip install --user docopt black rich
+    python -m pip install --user docopt black rich textual
     python -m pip install --user jupyterlab
 else
     echo "!!! python isn't installed; skipping installation of Python packages."
-fi
-
-if ! which xcape >/dev/null; then
-    echo ">>> Installing xcape [via github.com/alols/xcape]"
-    sudo dnf install git gcc make pkgconfig libX11-devel libXtst-devel libXi-devel
-    git clone https://github.com/alols/xcape.git
-    cd xcape
-    make
-    sudo make install
-    cd ..
-    rm -rf ./xcape
-    if which xcape >/dev/null; then
-        echo ">>> Successfully installed xcape"
-    fi
 fi
