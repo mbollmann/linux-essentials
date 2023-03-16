a5d103229a0e470e619868dfaf96c5d8e2ab3f85
Marcel Bollmann <marcel@bollmann.me>
2023-03-16 18:58:53 +0100

    Improved syncing and diffing

--- diff-dotfiles.sh
+++ diff-dotfiles.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 echo_status() {
-    echo -e "\033[1m\033[32m*** \033[94m$1\033[0m"
+    echo -e "\033[1m\033[32m***\033[94m" $* "\033[0m"
 }
 
-diff() {
-    echo_status "Diffing dot-files in home directory"
+git_diff() {
+    echo_status "Diffing dot-files between" ~ and "repository"
     cd dot-files/
-    find . -type f -name ".*" -exec diff -q {} ~/{} \;
-    echo_status "Diffing SSH configuration"
-    find .ssh/ -type f -exec diff -q {} ~/{} \;
-    echo_status "Diffing application-specific configuration"
-    find .config/ -type f -exec diff -q {} ~/{} \;
-    echo_status "Diffing Emacs configuration"
+    find . -type f -name ".*" -exec git diff --color=always {} ~/{} \;
+    find .ssh/ -type f -exec git diff --color=always {} ~/{} \;
+    find .config/ -type f -exec git diff --color=always {} ~/{} \;
     find   .emacs.d/ -not -path '*/auto-save-list*'\
                      -not -path '*/my-desktop-sessions*'\
                      -not -name 'tramp'\
@@ -21,7 +18,48 @@ diff() {
                      -not -path '*.emacs.d/'\
                      -not -path '*/elpa*'\
                      -type f\
-                     -exec diff -q {} ~/{} \;
+                     -exec git diff --color=always {} ~/{} \;
+    cd ..
 }
 
-diff
+norm_diff() {
+    echo_status "Diffing dot-files between" ~ and "repository"
+    cd dot-files/
+    find . -type f -name ".*" -exec diff {} ~/{} \;
+    find .ssh/ -type f -exec diff {} ~/{} \;
+    find .config/ -type f -exec diff {} ~/{} \;
+    find   .emacs.d/ -not -path '*/auto-save-list*'\
+                     -not -path '*/my-desktop-sessions*'\
+                     -not -name 'tramp'\
+                     -not -name 'bookmarks'\
+                     -not -name 'ac-comphist.dat'\
+                     -not -path '*.emacs.d/'\
+                     -not -path '*/elpa*'\
+                     -type f\
+                     -exec diff {} ~/{} \;
+    cd ..
+}
+
+check_for_missing() {
+    echo_status "Brief diff, including missing files"
+    cd dot-files/
+    find .config/ -mindepth 1 -prune -type d -exec diff -rq ~/{} {} \;
+    find   .emacs.d/ -not -path '*/auto-save-list*'\
+                     -not -path '*/my-desktop-sessions*'\
+                     -not -name 'tramp'\
+                     -not -name 'bookmarks'\
+                     -not -name 'ac-comphist.dat'\
+                     -not -path '*.emacs.d/'\
+                     -not -path '*/elpa*'\
+                     -type f\
+                     -exec diff -rq {} ~/{} \;
+    cd ..
+}
+
+if which git >/dev/null; then
+    git_diff
+else
+    norm_diff
+fi
+
+check_for_missing
--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -6,20 +6,23 @@ echo_status() {
 
 deploy() {
     echo_status "Syncing dot-files"
-    rsync -au dot-files/ ~/
+    rsync -au --exclude-from=.sync-exclude dot-files/ ~/
+
+    if ! rsync -ni -avu --include-from=.sync-exclude --exclude="*" dot-files/ ~/ | grep -q "total size is 0 " ; then
+        echo -e "--> \033[31m\033[1mThere are files that should be synced but can't\033[0m, because they contain secrets. "
+        rsync -ni -avu --include-from=.sync-exclude --exclude="*" dot-files/ ~/
+    fi
 }
 
 check_for_newer() {
-    echo_status "Checking for dot-files with changes that are not in the repo"
+    echo_status "Checking for dot-files that differ between user directory and repo"
     cd dot-files/
     find . -type f -exec diff -q ~/{} {} \;
-    find .config/ -mindepth 1 -prune -type d -exec diff -r ~/{} {} \;
-    diff -r ~/.emacs.d/ .emacs.d/
     cd ..
 }
 
 # Dry-run
-rsync -ni -avu dot-files/ ~/
+rsync -ni -avu --exclude-from=.sync-exclude dot-files/ ~/
 
 echo ""
 echo -e "\033[1m\033[4mSync changes above to ~/?\033[0m"
