58f91959b3be23a122587e46634803cd18d7ab71
Marcel Bollmann <marcel@bollmann.me>
2023-03-28 16:04:17 +0200

    Add fish abbreviations

--- dot-files/.config/fish/conf.d/_abbr.fish
+++ dot-files/.config/fish/conf.d/_abbr.fish
@@ -1,7 +1,14 @@
+abbr -a gcl git clone
+abbr -a gcg --set-cursor "git clone github:%"
 abbr -a gco git checkout
-abbr -a gpl git pull
+abbr -a gp git pull
 abbr -a gpr git pull --rebase
-abbr -a gps git push
+abbr -a gu git push
+abbr -a gs git status
 abbr -a ls exa
+abbr -a cat "bat -p"
 abbr -a open --set-cursor "open % 2>/dev/null"
 abbr -a sus --set-cursor "sort % | uniq -c | sort -nr"
+abbr -a pipi pip install
+abbr -a pipu pip install --upgrade
+abbr -a replace-in-last-command --regex "^\^[^\^]+\^[^\^]+" --function __mmb_replace_in_last_cmd
