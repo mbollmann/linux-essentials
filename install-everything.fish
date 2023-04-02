8e2b4b2bd3c5f57106c74c1a156e14c7216bf316
Marcel Bollmann <marcel@bollmann.me>
2023-04-02 11:47:26 +0200

    Add comments

--- install-everything.fish
+++ install-everything.fish
@@ -5,23 +5,25 @@ if fish_is_root_user
     exit 1
 end
 
+# Install dnf packages
 for packages in (dirname (status --current-filename))/pkg-fedora/*.txt
+    # Don't use --assumeyes so we can decide whether to install a particular
+    # collection of packages or not
     sudo dnf install (cat $packages)
 end
 
+# Install other software
 for script in (dirname (status --current-filename))/install/*.fish
-    if string match --regex "^_" --quiet (basename $script)
-        continue
-    end
-
     # If script is not executable, we don't want to install it by default
     if test -x "$script"
         fish $script
     end
 end
 
+# Sync all dot-files
 bash (dirname (status --current-filename))/sync-dotfiles.sh
 
+# Enable custom systemd services
 if test -f ".config/systemd/user/emacs-256color.service"
-    systemctl --user enable emacs-256color
+    systemctl --user start emacs-256color ; and systemctl --user enable emacs-256color
 end
